b7d9b29edf89bc69984e15fc1a4a1d30291b44a6
Tanguy DUMAS <tdumas@aubay.com>
2026-06-10 10:52:12 +0200

    fix: test de log pour voir si les variables d'env sont bien injectees avec vercel

--- app/plugins/01.supabase.client.ts
+++ app/plugins/01.supabase.client.ts
@@ -4,9 +4,19 @@ import { defineNuxtPlugin, useRuntimeConfig } from 'nuxt/app'
 export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig()
 
+  console.log(
+    'URL:',
+    config.public.supabaseUrl
+  )
+
+  console.log(
+    'KEY:',
+    config.public.supabaseAnonKey
+  )
+  
   const supabase = createClient(
-    config.public.supabaseUrl as string,
-    config.public.supabaseAnonKey as string
+    config.public.supabaseUrl,
+    config.public.supabaseAnonKey
   )
 
   return {
